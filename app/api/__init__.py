fff52e07966ae6b07da6c5ba32898d7134a3272a
nothern05 <gleb5shimko@gmail.com>
2025-01-10 15:19:55 +0200

    update msg

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .auth.views import router as auth_router
 from .chats.views import router as chats_router
+from .messages.views import router as msg_router
 
 router = APIRouter()
 
 router.include_router(router=auth_router, prefix="/auth")
 router.include_router(router=chats_router, prefix="/chats")
+router.include_router(router=msg_router, prefix="/messages")
